2d964129aeed9550593748867189a81e9a1530a7
mwmiller75 <mwmiller75@gmail.com>
2018-03-05 14:52:22 -0700

    ListRoutes.js completed

---
+++ server/routes/listRoutes.js
@@ -0,0 +1,39 @@
+var router = require("express").Router()
+var Lists = require("../models/list")
+
+//createList
+
+router.post("/api/boards/:boardId/lists", (req, res, next) => {
+    Lists.create(req.body)
+        .then(list => {
+            res.send(list)
+        })
+        .catch(next)
+})
+
+// deleteList
+router.delete("/api/boards/:boardId/lists/:listId", (req, res, next) => {
+    Lists.findByIdAndRemove(req.params.listId)
+        .then(comment => {
+            res.send({ message: "Successfully deleted list" })
+        })
+        .catch(next)
+})
+
+// updateList  (put)
+
+router.put("/api/boards/:boardId/lists/:listId", (req, res, next) => {
+    Lists.findByIdAndUpdate(req.params.listId, req.body, { new: true })
+        .then(comment => {
+            res.send({ message: "Successfully updated list", data: comment })
+        })
+        .catch(next)
+})
+
+// getListsByBoardId
+router.get("/api/boards/:boardId/lists", (req, res, next) => {
+    Lists.find({ boardId: req.params.boardId })
+        .then(lists => {
+            return res.send(lists)
+        })
+})
