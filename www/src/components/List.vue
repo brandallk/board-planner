2c45c11bfdbaae074b1f4e48d80f7f47939b4eba
mwmiller75 <mwmiller75@gmail.com>
2018-03-09 11:46:28 -0700

    Removed prop for 'board', kept computed view

--- www/src/components/List.vue
+++ www/src/components/List.vue
@@ -40,8 +40,7 @@
             taskCard: TaskCard
         },
         props: [
-            'list',
-            'board'
+            'list'
         ],
         data() {
             return {
@@ -57,9 +56,9 @@
             }
         },
         computed: {
-            // board() {
-            //     return this.$store.state.activeBoard
-            // },
+            board() {
+                return this.$store.state.activeBoard
+            },
             listTasks() {
                 var boardTasks = this.$store.state.boardTasks
                 return boardTasks.filter(task => task.listId === this.list._id)
